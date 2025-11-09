038972f3d90081b1d5761d208d5dab83a1471209
milenaa052 <milenasantosdeoliveira40@gmail.com>
2025-11-09 01:34:03 -0300

    feat: creating services connection

--- scheduling-service/src/app.module.ts
+++ scheduling-service/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SchedulingCompanyModule } from './scheduling-company/schedulingCompany.module';
+import { SchedulingCustomerModule } from './scheduling-customer/schedulingCustomer.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5434,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'postgres',
+      database: process.env.DB_NAME || 'schedulingdb',
+      autoLoadModels: true,
+      synchronize: true,
+      logging: false,
+    }),
+    SchedulingCompanyModule,
+    SchedulingCustomerModule
+  ],
 })
 export class AppModule {}
