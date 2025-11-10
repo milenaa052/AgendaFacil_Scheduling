ae4964128e0d6760e7dfdd4de591ad45198595fd
milenaa052 <milenasantosdeoliveira40@gmail.com>
2025-11-09 22:40:09 -0300

    feat: creating token validation

--- scheduling-service/src/app.module.ts
+++ scheduling-service/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { SchedulingCompanyModule } from './scheduling-company/schedulingCompany.module';
 import { SchedulingCustomerModule } from './scheduling-customer/schedulingCustomer.module';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -21,8 +23,15 @@ import { SchedulingCustomerModule } from './scheduling-customer/schedulingCustom
       synchronize: true,
       logging: false,
     }),
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' }
+    }),
+
     SchedulingCompanyModule,
     SchedulingCustomerModule
   ],
+  providers: [JwtStrategy]
 })
 export class AppModule {}
