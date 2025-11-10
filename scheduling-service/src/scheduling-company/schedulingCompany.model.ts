573abd096e7636e1bff9e56b2e5a159c7d7b24f5
milenaa052 <milenasantosdeoliveira40@gmail.com>
2025-11-10 18:53:00 -0300

    fix: changing date types to string

--- scheduling-service/src/scheduling-company/schedulingCompany.model.ts
+++ scheduling-service/src/scheduling-company/schedulingCompany.model.ts
@@ -10,8 +10,8 @@ export interface SchedulingCompanyCreationAttributes {
     companyId: number;
     customerId: number;
     title: string;
-    startDate: Date;
-    endDate: Date;
+    startDate: string;
+    endDate: string;
     startHour: string;
     endHour: string;
     status: SchedulingCompanyStatus;
@@ -48,16 +48,16 @@ export class SchedulingCompany extends Model<SchedulingCompany, SchedulingCompan
     declare title: string;
 
     @Column({ 
-        type: DataType.DATE,
+        type: DataType.STRING,
         allowNull: false 
     })
-    declare startDate: Date;
+    declare startDate: string;
 
     @Column({ 
-        type: DataType.DATE,
+        type: DataType.STRING,
         allowNull: false 
     })
-    declare endDate: Date;
+    declare endDate: string;
 
     @Column({ 
         type: DataType.STRING,
